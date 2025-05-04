034f2d8826ae1a10a1f2c71ca150ff4c6ca484ba
boomchanotai <boom9387@hotmail.com>
2025-05-04 21:42:14 +0700

    fix: dag

--- airflow-workflows/dags/weather_history_dag.py
+++ airflow-workflows/dags/weather_history_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators import PythonOperator
+from airflow.operators.python import PythonOperator  # type: ignore
 from datetime import datetime, timedelta
 import sys
 import os
