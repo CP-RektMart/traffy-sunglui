f6170f069ed77d800b6fa741b70b9adf00c59601
boomchanotai <boom9387@hotmail.com>
2025-05-04 19:15:53 +0700

    fix: support DAG

--- data-eng/big-query/fetch-new-traffy.py
+++ data-eng/big-query/fetch-new-traffy.py
@@ -5,115 +5,122 @@ import pandas as pd
 from queryclient import client, check_existing_ticket_ids, table_ref, dataset_id, table_id, schema, get_last_existing_timestamp
 from utils import convert_to_utc  # Ensure this converts to UTC timestamp
 
-# URL of the API endpoint
-url = "https://publicapi.traffy.in.th/teamchadchart-stat-api/geojson/v1?limit=1000&offset={offset}"
 
-# Rate limit: 100 requests per minute (0.6 seconds per request)
-rate_limit_delay = 60 / 100
+def run():
+    # URL of the API endpoint
+    url = "https://publicapi.traffy.in.th/teamchadchart-stat-api/geojson/v1?limit=1000&offset={offset}"
 
-# Offset for pagination
-offset = 0
-has_new_data = True
+    # Rate limit: 100 requests per minute (0.6 seconds per request)
+    rate_limit_delay = 60 / 100
 
-# Job configuration for BigQuery load
-job_config = bigquery.LoadJobConfig(
-    write_disposition="WRITE_APPEND",
-    autodetect=True,
-    schema=schema,
-)
+    # Offset for pagination
+    offset = 0
+    has_new_data = True
 
-# Get the last timestamp from BigQuery
-last_existing_timestamp = get_last_existing_timestamp()
+    # Job configuration for BigQuery load
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+        autodetect=True,
+        schema=schema,
+    )
 
-if last_existing_timestamp is None:
-    print("No data found in BigQuery. Fetching all data from the API.")
-else:
-    print(f"Last existing timestamp in BigQuery: {last_existing_timestamp}")
+    # Get the last timestamp from BigQuery
+    last_existing_timestamp = get_last_existing_timestamp()
 
-# Convert last_existing_timestamp to UTC for comparison
-last_existing_timestamp = convert_to_utc(
-    last_existing_timestamp) if last_existing_timestamp else None
-
-while has_new_data:
-    # Send GET request to the API
-    response = requests.get(url.format(offset=offset))
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response JSON into a Python dictionary
-        data = response.json()
-
-        # Extract ticket_ids from the fetched data
-        ticket_ids = [feature["properties"]["ticket_id"]
-                      for feature in data["features"]]
-
-        # Check if any of the fetched ticket_ids already exist in BigQuery
-        existing_ticket_ids = check_existing_ticket_ids(
-            dataset_id, table_id, ticket_ids)
-
-        # Filter out existing data and prepare the rows for BigQuery
-        new_rows = []
-        new_data_timestamps = []
-
-        for feature in data["features"]:
-            ticket_id = feature["properties"]["ticket_id"]
-            if ticket_id not in existing_ticket_ids:
-                # Prepare the row for BigQuery
-                properties = feature["properties"]
-                geometry = feature["geometry"]
-                row = {
-                    "ticket_id": ticket_id,
-                    "type": "{" + ", ".join([org.strip() for org in properties["org"]]).replace(" ", "") + "}",
-                    "organization": ", ".join(properties["org"]),
-                    "comment": properties["description"],
-                    "photo": properties["photo_url"],
-                    "photo_after": properties["after_photo"],
-                    "coords": f"{geometry['coordinates'][1]},{geometry['coordinates'][0]}",
-                    "address": properties["address"],
-                    "subdistrict": properties["subdistrict"],
-                    "district": properties["district"],
-                    "province": properties["province"],
-                    "timestamp": convert_to_utc(properties["timestamp"]) if properties["timestamp"] else None,
-                    "state": properties["state"],
-                    "star": properties["star"] if properties["star"] is not None else None,
-                    "count_reopen": properties["count_reopen"],
-                    "last_activity": convert_to_utc(properties["last_activity"]) if properties["last_activity"] else None
-                }
-                new_rows.append(row)
-                new_data_timestamps.append(properties["timestamp"])
-
-        if new_rows:
-            # Convert new rows to DataFrame
-            new_df = pd.DataFrame(new_rows)
-
-            # Upload the data directly to BigQuery
-            job = client.load_table_from_dataframe(
-                new_df, table_ref, job_config=job_config)
-            job.result()  # Wait for the job to finish
-            print(f"Inserted {len(new_df)} rows.")
-
-            # Get the latest timestamp from the new data
-            latest_new_data_timestamp = max(new_data_timestamps)
-
-            # Convert latest_new_data_timestamp to UTC
-            latest_new_data_timestamp = convert_to_utc(
-                latest_new_data_timestamp) if latest_new_data_timestamp else None
-
-            # Check if the latest new timestamp is older than or equal to the last existing timestamp
-            if latest_new_data_timestamp and last_existing_timestamp and latest_new_data_timestamp <= last_existing_timestamp:
-                print("Detected overlap. Stopping fetch.")
-                break
-
-            # Update the offset for the next batch of data
-            offset += 1000
-        else:
-            # No new data, stop fetching
-            has_new_data = False
-
+    if last_existing_timestamp is None:
+        print("No data found in BigQuery. Fetching all data from the API.")
     else:
         print(
-            f"Failed to fetch data. HTTP Status code: {response.status_code}")
-        break
+            f"Last existing timestamp in BigQuery: {last_existing_timestamp}")
 
-    # Sleep to respect the rate limit (100 requests per minute)
-    time.sleep(rate_limit_delay)
+    # Convert last_existing_timestamp to UTC for comparison
+    last_existing_timestamp = convert_to_utc(
+        last_existing_timestamp) if last_existing_timestamp else None
+
+    while has_new_data:
+        # Send GET request to the API
+        response = requests.get(url.format(offset=offset))
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the response JSON into a Python dictionary
+            data = response.json()
+
+            # Extract ticket_ids from the fetched data
+            ticket_ids = [feature["properties"]["ticket_id"]
+                          for feature in data["features"]]
+
+            # Check if any of the fetched ticket_ids already exist in BigQuery
+            existing_ticket_ids = check_existing_ticket_ids(
+                dataset_id, table_id, ticket_ids)
+
+            # Filter out existing data and prepare the rows for BigQuery
+            new_rows = []
+            new_data_timestamps = []
+
+            for feature in data["features"]:
+                ticket_id = feature["properties"]["ticket_id"]
+                if ticket_id not in existing_ticket_ids:
+                    # Prepare the row for BigQuery
+                    properties = feature["properties"]
+                    geometry = feature["geometry"]
+                    row = {
+                        "ticket_id": ticket_id,
+                        "type": "{" + ", ".join([org.strip() for org in properties["org"]]).replace(" ", "") + "}",
+                        "organization": ", ".join(properties["org"]),
+                        "comment": properties["description"],
+                        "photo": properties["photo_url"],
+                        "photo_after": properties["after_photo"],
+                        "coords": f"{geometry['coordinates'][1]},{geometry['coordinates'][0]}",
+                        "address": properties["address"],
+                        "subdistrict": properties["subdistrict"],
+                        "district": properties["district"],
+                        "province": properties["province"],
+                        "timestamp": convert_to_utc(properties["timestamp"]) if properties["timestamp"] else None,
+                        "state": properties["state"],
+                        "star": properties["star"] if properties["star"] is not None else None,
+                        "count_reopen": properties["count_reopen"],
+                        "last_activity": convert_to_utc(properties["last_activity"]) if properties["last_activity"] else None
+                    }
+                    new_rows.append(row)
+                    new_data_timestamps.append(properties["timestamp"])
+
+            if new_rows:
+                # Convert new rows to DataFrame
+                new_df = pd.DataFrame(new_rows)
+
+                # Upload the data directly to BigQuery
+                job = client.load_table_from_dataframe(
+                    new_df, table_ref, job_config=job_config)
+                job.result()  # Wait for the job to finish
+                print(f"Inserted {len(new_df)} rows.")
+
+                # Get the latest timestamp from the new data
+                latest_new_data_timestamp = max(new_data_timestamps)
+
+                # Convert latest_new_data_timestamp to UTC
+                latest_new_data_timestamp = convert_to_utc(
+                    latest_new_data_timestamp) if latest_new_data_timestamp else None
+
+                # Check if the latest new timestamp is older than or equal to the last existing timestamp
+                if latest_new_data_timestamp and last_existing_timestamp and latest_new_data_timestamp <= last_existing_timestamp:
+                    print("Detected overlap. Stopping fetch.")
+                    break
+
+                # Update the offset for the next batch of data
+                offset += 1000
+            else:
+                # No new data, stop fetching
+                has_new_data = False
+
+        else:
+            print(
+                f"Failed to fetch data. HTTP Status code: {response.status_code}")
+            break
+
+        # Sleep to respect the rate limit (100 requests per minute)
+        time.sleep(rate_limit_delay)
+
+
+if __name__ == "__main__":
+    run()
--- data-eng/weather-history/fetch-weather-history.py
+++ data-eng/weather-history/fetch-weather-history.py
@@ -2,73 +2,102 @@ import openmeteo_requests
 from datetime import datetime
 import pandas as pd
 from google.cloud import bigquery
-from queryclient import client, table_ref, schema
+from queryclient import client, project_id, dataset_id, table_id, table_ref, schema
 
 
-# Settings
-LATITUDE = 13.7563
-LONGTITUDE = 100.5018
+def run():
+    # Settings
+    LATITUDE = 13.7563
+    LONGTITUDE = 100.5018
 
-START_DATE = "2021-08-01"
-END_DATE = datetime.now().strftime("%Y-%m-%d")
+    START_DATE = "2021-08-01"
+    END_DATE = datetime.now().strftime("%Y-%m-%d")
 
-RAIN_SUM_THRESHOLD = 1  # mm
-PRECIPITATION_HOURS_THRESHOLD = 3  # hours
+    RAIN_SUM_THRESHOLD = 1  # mm
+    PRECIPITATION_HOURS_THRESHOLD = 3  # hours
 
-openmeteo = openmeteo_requests.Client()
+    openmeteo = openmeteo_requests.Client()
 
-url = "https://archive-api.open-meteo.com/v1/archive"
-params = {
-    "latitude": LATITUDE,
-    "longitude": LONGTITUDE,
-    "start_date": START_DATE,
-    "end_date": END_DATE,
-    "daily": ["rain_sum", "precipitation_hours", "precipitation_sum"],
-    "timezone": "Asia/Bangkok",
-}
-responses = openmeteo.weather_api(url, params=params)
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": LATITUDE,
+        "longitude": LONGTITUDE,
+        "start_date": START_DATE,
+        "end_date": END_DATE,
+        "daily": ["rain_sum", "precipitation_hours", "precipitation_sum"],
+        "timezone": "Asia/Bangkok",
+    }
+    responses = openmeteo.weather_api(url, params=params)
 
-response = responses[0]
-print(f"Getting rain data for Bangkok at {response.Latitude():.2f}°N, {response.Longitude():.2f}°E "
-      f"(Elevation: {response.Elevation():.0f} m asl, Timezone: {response.Timezone().decode()} {response.TimezoneAbbreviation().decode()}, "
-      f"UTC offset: {response.UtcOffsetSeconds()}s)")
+    response = responses[0]
+    print(f"Getting rain data for Bangkok at {response.Latitude():.2f}°N, {response.Longitude():.2f}°E "
+          f"(Elevation: {response.Elevation():.0f} m asl, Timezone: {response.Timezone().decode()} {response.TimezoneAbbreviation().decode()}, "
+          f"UTC offset: {response.UtcOffsetSeconds()}s)")
 
-daily = response.Daily()
-daily_rain_sum = daily.Variables(0).ValuesAsNumpy()
-daily_precipitation_hours = daily.Variables(1).ValuesAsNumpy()
-daily_precipitation_sum = daily.Variables(2).ValuesAsNumpy()
+    daily = response.Daily()
+    daily_rain_sum = daily.Variables(0).ValuesAsNumpy()
+    daily_precipitation_hours = daily.Variables(1).ValuesAsNumpy()
+    daily_precipitation_sum = daily.Variables(2).ValuesAsNumpy()
 
-daily_data = {
-    "date": pd.date_range(
-        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-        end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-        freq=pd.Timedelta(seconds=daily.Interval()),
-        inclusive="left",
+    daily_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+            end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=daily.Interval()),
+            inclusive="left",
+        )
+    }
+
+    daily_data["rain_sum"] = daily_rain_sum
+    daily_data["precipitation_hours"] = daily_precipitation_hours
+    daily_data["precipitation_sum"] = daily_precipitation_sum
+
+    rain_df = pd.DataFrame(data=daily_data)
+
+    rain_df['is_rain'] = (rain_df['rain_sum'] >= RAIN_SUM_THRESHOLD) & (
+        rain_df['precipitation_hours'] >= PRECIPITATION_HOURS_THRESHOLD)
+
+    rain_df['date'] = pd.to_datetime(rain_df["date"]).dt.date
+
+    # === Query max date in BigQuery ===
+    try:
+        query = f"""
+        SELECT MAX(date) AS max_date
+        FROM `{project_id}.{dataset_id}.{table_id}`
+        """
+        max_date_result = client.query(query).result()
+        max_date_row = next(max_date_result, None)
+        max_date = max_date_row.max_date if max_date_row else None
+    except Exception as e:
+        print(f"Error querying max date: {e}")
+        max_date = None
+
+    # === Filter rain_df based on max_date ===
+    if max_date:
+        rain_df = rain_df[rain_df["date"] > max_date]
+        print(
+            f"Filtered data to dates after {max_date}. Remaining rows: {len(rain_df)}")
+    else:
+        print("Table is empty or not found. Appending all rows.")
+
+    if rain_df.empty:
+        print("No new data to insert.")
+        return
+
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND",
+        schema=schema
     )
-}
 
-daily_data["rain_sum"] = daily_rain_sum
-daily_data["precipitation_hours"] = daily_precipitation_hours
-daily_data["precipitation_sum"] = daily_precipitation_sum
+    # Upload to BigQuery
+    job = client.load_table_from_dataframe(
+        rain_df,
+        table_ref,
+        job_config=job_config
+    )
+    job.result()  # Waits for the job to complete
+    print(f"Inserted {len(rain_df)} new rows of rain data into BigQuery.")
 
-rain_df = pd.DataFrame(data=daily_data)
 
-rain_df['is_rain'] = (rain_df['rain_sum'] >= RAIN_SUM_THRESHOLD) & (
-    rain_df['precipitation_hours'] >= PRECIPITATION_HOURS_THRESHOLD)
-rain_df['is_rain'].value_counts()
-
-rain_df['date'] = pd.to_datetime(rain_df["date"]).dt.date
-
-job_config = bigquery.LoadJobConfig(
-    write_disposition="WRITE_APPEND",
-    schema=schema
-)
-
-# Upload to BigQuery
-job = client.load_table_from_dataframe(
-    rain_df,
-    table_ref,
-    job_config=job_config
-)
-job.result()  # Waits for the job to complete
-print(f"Inserted {len(rain_df)} rows of rain data into BigQuery.")
+if __name__ == "__main__":
+    run()
